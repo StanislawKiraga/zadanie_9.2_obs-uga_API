bd00205b93756cfaa3b348da06781820b097e42d
StanislawKiraga <kiragasta@gmail.com>
2023-03-31 16:01:04 +0200

    ...

--- kalkulator_walut.py
+++ kalkulator_walut.py
@@ -1,33 +1,43 @@
 import requests
 import csv
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, request
+
+
+def get_data_from_nbp():
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+    data = response.json()
+    rates = data[0]['rates']
+    return rates
+
+
+def export_data_to_csv(data, file_name='data.csv'):
+    with open(file_name) as f:
+        writer = csv.DictWriter(f)
+        writer.writerows(data)
+
+
+def calculate(data, amount, currency):
+    rate = [d for d in data if d["code"] == currency][0]["ask"]
+    return amount * rate
 
 app = Flask(__name__)
 
 
-response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()
-rates = data[0]['rates']
-headers = rates[0].keys()
+@app.route('/', methods=['GET', 'POST'])
+def calculator():
+    data = get_data_from_nbp()
 
-
-@app.route('/home')
-def home():
-    return render_template('base.htm')
-
-
-@app.route('/recalculate', methods=['GET', 'POST'])
-def recalculate():
     if request.method == 'POST':
-        
+        currency = request.form['currency']
+        amount = request.form['amount']
+        amount = float(amount)
+        result = calculate(data, amount, currency)
+        return render_template('base.htm', result=result, data=data)
     
-        
-with open('/Users/amitrosz-wromac/Desktop/Kodilla/Flask/API/rates.csv', 'w') as f:
-    writer = csv.DictWriter(f, fieldnames=headers, delimiter=';')
-    writer.writeheader()
-    writer.writerows(rates)
+    return render_template('base.htm', result=None, data=data)
+    
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
-           
+
+
